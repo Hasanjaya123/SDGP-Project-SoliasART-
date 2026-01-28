8ed9970bef3bc4a4e9c477cbbabe3e9f9e073ede
Thinal2004 <thinalprabasha@gmail.com>
2026-01-29 00:01:01 +0530

    feature Login : implement JWT token generation and configuration

--- backend/app/modules/auth/utils.py
+++ backend/app/modules/auth/utils.py
@@ -1,5 +1,10 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from app.core.config import settings
+from datetime import datetime, timedelta, timezone
 
+# Setup password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
@@ -7,3 +12,16 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    # Create JWT Token
+    to_encode = data.copy()
+    
+    # Calculate when the token expires (Now + 30 mins)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    # Sign the token using your SECRET_KEY from config.py
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    
+    return encoded_jwt
