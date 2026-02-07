92f5ba894b39242b3329cf56c311989429b33ca9
Sevin Kawsika <saween.kausika123@gmail.com>
2026-02-07 09:35:14 +0530

    Fix possition picture and only scroll forum

--- frontend/SoliasArt/src/pages/LoginPage.jsx
+++ frontend/SoliasArt/src/pages/LoginPage.jsx
@@ -179,7 +179,7 @@ const LoginPage = () => {
                 </div>
             </div>
             {/* RIGHT SIDE: The Image (Width 50%) */}
-                <div className="hidden w-1/2 md:block">
+                <div className="hidden w-1/2 md:flex md:fixed md:right-0 md:top-0 md:h-screen md:items-center md:justify-center">
                     <img 
                     src="./src/assets/Sign In.jpg" 
                     alt="Decoration" 
