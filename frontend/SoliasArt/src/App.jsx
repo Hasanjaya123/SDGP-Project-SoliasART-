b466d36294e06c1f3b4c6a3afc678224a20d4962
Hasanjaya perera <vibercat723@gmail.com>
2026-02-10 18:15:27 +0530

    Feature/main components (#2)
    * Implemented signup backend
    
    * Updated signup feature and added comments
    
    * feature Login : implement JWT token generation and configuration
    
    * add .gitignore to backend/.venv file
    
    * Removing additional main.py
    
    * completed the login implementation
    
    * add react icons and remove some peer: true
    
    * remove display and place-item in body and chnage the a and button colour to this D97706
    
    * import and add SignupPage
    
    * login endpoint bug fixed
    
    * update the requirements.txt file
    
    * create SignupPage skeleton
    
    * state definition and create loarding and error useState
    
    * add get_current_user dependency and /me endpoint
    
    * add commets and create logic in SignUp page
    
    * add all inputs to the signup Page
    
    * change the image style to keep fixed posstion and only scroll forum
    
    * Added name and method attributes to the form and Added autocomplete attributes to all inputs
    
    * Fix autofill inputs turn into grey colour issue
    
    * connect frontend signuppsge with backend database
    
    * Install react-router-dom
    
    * add react-router-dom and recreate the structure base on router
    
    * change the image round to 20px
    
    * Add the forum and add picture to LoginPage
    
    * Add rules to the LoginPage
    
    * Fix possition picture and only scroll forum
    
    * Change forgot password tocamel case format
    
    * Fixed signup logic frontend
    
    * updated the LoginPage.jsx - save the token in local storage
    
    * Craeted a new .env file
    
    * Removed the .env file
    
    * Updated gitignore to hide .env file
    
    * Created nav bar
    
    * Created ArtDisplay Card
    
    * Created Footer Component
    
    ---------
    
    Co-authored-by: Sevin Kawsika <saween.kausika123@gmail.com>

--- frontend/SoliasArt/src/App.jsx
+++ frontend/SoliasArt/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignupPage from './pages/SignupPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import './index.css';
+import Test from './pages/test.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
+
         {/* Route to Signup page */}
         <Route path="/signup" element={<SignupPage />} />
         
         {/* Route to Login page */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Test route for ArtDisplayCard */}
+        <Route path="/test" element={<Test />} />
         
         {/* Default route - redirect to signup */}
         <Route path="/" element={<Navigate to="/signup" replace />} />
