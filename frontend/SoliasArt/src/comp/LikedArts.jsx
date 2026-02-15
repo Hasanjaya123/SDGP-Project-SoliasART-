dd14e2529847efc95f5ff3847499381a12ac66cd
voidshadow98 <janindunanayakkara96@gmail.com>
2026-02-15 10:58:25 +0530

    created the LikedArts component

---
+++ frontend/SoliasArt/src/comp/LikedArts.jsx
@@ -0,0 +1,17 @@
+import ArtCard from './ArtCard';
+
+function LikedArtS({LikedArtS}) {
+    return (
+        <section className='liked-arts'>
+            <h2>Your Liked Arts</h2>
+            <div className='arts-grind'>
+                {LikedArtS.map(((art) => (
+                    <ArtCard key={art.id} title={art.title} image={art.image} />
+
+                )))}
+            </div>
+        </section>
+
+        
+    )
+}
