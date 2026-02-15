7ca1b5357c97e519ffaeb63aea69f038aa831058
voidshadow98 <janindunanayakkara96@gmail.com>
2026-02-15 10:50:24 +0530

    created the BoardList component

---
+++ frontend/SoliasArt/src/comp/BoardList.jsx
@@ -0,0 +1,26 @@
+import BoardCard from'./BoardCard';
+import CreateCard from'./CreateCard';
+
+function BoardList({ boards, onCreate }) {
+    return (
+        <section className='saved-boards'>
+            {boards.map((board) => (
+                <BoardCard 
+                key={board.id}
+                title={board.title}
+                pinCount={board.pinCount}
+                lastUpdated={board.lastUpdated}
+                image={board.image}
+                
+                
+                />
+            ))}
+            <CreateCard onCreate={onCreate} />
+            </section>
+
+            
+            
+        
+    );
+
+}
