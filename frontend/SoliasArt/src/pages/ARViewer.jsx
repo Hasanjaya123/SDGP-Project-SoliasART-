46fb0dae240ca5fdeac804c76ec3f5de88f69034
Sevin Kawsika <saween.kausika123@gmail.com>
2026-03-08 11:02:57 +0530

    Generate QR code and pointing to backend GLB cache URL

--- frontend/SoliasArt/src/pages/ARViewer.jsx
+++ frontend/SoliasArt/src/pages/ARViewer.jsx
@@ -15,10 +15,32 @@ import { useSearchParams } from "react-router-dom";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
+  function QRCode({url}){
+    // encode the URL so it can be safely included in the QR code
+    const encodedUrl = encodeURIComponent(url);
+
+    return (
+      // Use to generate the QR code image 
+      <img
+        src={`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodedUrl}`}
+        alt="QR Code"
+      />
+    );
+  }
+
 export default function ARViewer() {
   const [artworkid, setArtworkid] = useState("");
   const [isLoading, setIsLoading] = useState(false); // true while waiting for backend response
   const [errorMsg, setErrorMsg] = useState(""); // stores error text if something fails
+  const [qrReady, setQrReady] = useState(false); // becomes true show QR code
+
+  const backendGlbUrl = artworkid.trim() 
+  ? '${BACKEND_URL}/ar/generate-ar/${artworkid.trim()}' 
+  : null;
+
+  const mobilePreviewUrl = backendGlbUrl
+    ? `${window.location.origin}/preview?glb=${encodeURIComponent(backendGlbUrl)}`
+    : null;
 
   // this function work if the user clicks "Generate QR Code"
   const handleGenerate = async () => {
@@ -29,6 +51,7 @@ export default function ARViewer() {
     // reset error and show loading spinner on button
     setIsLoading(true);
     setErrorMsg(""); //show the loarding spinner on button
+    setQrReady(false); // hide QR code until we confirm backend has cached the GLB
 
     try {
       const res = await fetch(`${BACKEND_URL}/ar/generate-ar/${id}`, {
@@ -46,6 +69,7 @@ export default function ARViewer() {
       await res.blob(); // we don't actually care about the GLB data, just want to trigger the backend processing and caching
 
       alert("Success! GLB file fetched and cached by server")
+      setQrReady(true); // show QR code once backend has cached the GLB
 
     } catch (e) {
       // if any error occurs (network, server, parsing), show it to the user
@@ -78,6 +102,15 @@ export default function ARViewer() {
 
       {/* Only show error if there is one */}
       {errorMsg && <p style={{ color: "red" }}>{errorMsg}</p>}
+
+      {/* Show QR code once it's ready */}
+      {qrReady && mobilePreviewUrl &&(
+        <div>
+          <p>Scan the QR code to view the artwork in AR:</p>
+          <QRCode url={mobilePreviewUrl} />
+          <p>URL inside the QR code: {mobilePreviewUrl}</p>
+        </div>
+      )}
 
     </div>
   )
