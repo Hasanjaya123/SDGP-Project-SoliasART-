f8c0e8e52e2055daab7bc98202827a11515576ec
Sevin Kawsika <saween.kausika123@gmail.com>
2026-03-13 18:09:25 +0530

    add try exception to handel the foreign key vialation issue

--- backend/app/modules/Post/router.py
+++ backend/app/modules/Post/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, File, UploadFile, Form, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from fastapi.concurrency import run_in_threadpool
+from fastapi import APIRouter, File, UploadFile, Form, Depends, HTTPException
 from typing import Optional
 from uuid import UUID
 
@@ -32,16 +34,25 @@ async def create_post(
     tags=["post"],
     is_private_file=False)
 
-    # save post to database
-    new_post = Post(
-        artist_id = artist_id,
-        title = title,
-        description = description,
-        image_url = [upload_result.url],
-        likes = 0)
+    try:
+        # save post to database
+        new_post = Post(
+            artist_id = artist_id,
+            title = title,
+            description = description,
+            image_url = [upload_result.url],
+            likes = 0)
 
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
+        db.add(new_post)
+        db.commit()
+        db.refresh(new_post)
 
-    return {"id": new_post.id, "message": "Post created successfully"}
+        return {"id": new_post.id, "message": "Post created successfully"}
+
+    except IntegrityError:
+        db.rollback()
+        # if artist_id doesn't exist in artists table, hanndle the foreign key violation
+        raise HTTPException(
+            status_code=403,
+            detail="Only artists can upload posts"
+        )
