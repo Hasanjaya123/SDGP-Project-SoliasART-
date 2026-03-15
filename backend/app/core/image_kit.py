afc83fcb5e8784601d3b51d22828a14f7aa658b1
Nadeesha Senanayake <156213776+SANDS-003@users.noreply.github.com>
2026-03-16 03:18:32 +0530

    error handling

--- backend/app/core/image_kit.py
+++ backend/app/core/image_kit.py
@@ -4,6 +4,17 @@ from imagekitio import ImageKit
 
 load_dotenv()
 
-imagekit = ImageKit(
-    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY")
-)
+public_key = os.getenv("IMAGEKIT_PUBLIC_KEY")
+private_key = os.getenv("IMAGEKIT_PRIVATE_KEY")
+url_endpoint = os.getenv("IMAGEKIT_URL_ENDPOINT")
+
+imagekit = None
+
+if public_key and private_key and url_endpoint:
+    imagekit = ImageKit(
+        public_key=public_key,
+        private_key=private_key,
+        url_endpoint=url_endpoint
+    )
+else:
+    print("ImageKit not configured")
