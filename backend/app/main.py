a1489fc49d39ded875b3cef52b740544392a090d
Nadeesha Senanayake <156213776+SANDS-003@users.noreply.github.com>
2026-03-16 08:53:41 +0530

    back end error fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-﻿from fastapi import FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.modules.ArtUpload.router import router as art_upload_router
