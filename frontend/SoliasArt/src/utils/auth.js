425aefaa1a0aee477e7ec3e2bd99cf81a49b87b4
Sevin Kawsika <saween.kausika123@gmail.com>
2026-03-28 17:41:52 +0530

    add auth token function

--- frontend/SoliasArt/src/utils/auth.js
+++ frontend/SoliasArt/src/utils/auth.js
@@ -1,3 +1,7 @@
+export function getAuthToken() {
+    return localStorage.getItem('token')
+}
+
 export function getUserIdFromToken() {
     const token = localStorage.getItem('token')
     if (!token) return null
